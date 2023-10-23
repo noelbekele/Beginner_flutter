da40b9307bb763cd3da4cd4727b36e600b72b052
noelbekele <77620326+noelbekele@users.noreply.github.com>
2023-10-23 17:07:36 +0300

    Create row2.dart
    same but it is for cross axis alignment by:
            crossAxisAlignment: CrossAxisAlignment.etc,

---
+++ row2.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() =>runApp(MaterialApp(
+  home: Home(),
+
+));
+
+
+class Home extends StatelessWidget {
+  //const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Hi there noel '),
+        centerTitle: true,
+        backgroundColor: Colors.black12 ,
+
+      ),
+      body:Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('hello ther'),
+          TextButton(
+              onPressed: (){},
+              child: Text('click me'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(
+                  Colors.lightBlue,
+                ),
+              ),
+          ),
+          Container(
+            color: Colors.amber,
+            padding: EdgeInsets.all(30.0),
+            child: Text('inside a container'),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text('click+'),
+        backgroundColor: Colors.grey ,
+
+      ),
+    );
+  }
+}
